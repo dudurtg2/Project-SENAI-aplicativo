e0ca25f40b77f903f6263b48f229172bf587b3d2
Dudu! <83459496+dudurtg2@users.noreply.github.com>
2024-05-23 22:28:31 -0300

    mudança de telas

--- app/src/main/java/com/bora/LoginActivity.java
+++ app/src/main/java/com/bora/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.bora;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bora.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding; //ELIMINA A NECESSIDADE DO USO DO FINDBYID E LISTA TUDO QUE TEM NA ATIVIDADE/TELA
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.ClickCadastro.setOnClickListener(v -> {
+            startActivity(new Intent(this, CadastroActivity.class));
         });
+
     }
 }
--- app/src/main/java/com/bora/SplashActivily.java
+++ app/src/main/java/com/bora/SplashActivily.java
@@ -1,6 +1,8 @@
 package com.bora;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,9 @@ public class SplashActivily extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_activily);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        new Handler(getMainLooper()).postDelayed(()-> {
+            finish();
+            startActivity(new Intent(this,LoginActivity.class));
+        }, 2000);
     }
 }
