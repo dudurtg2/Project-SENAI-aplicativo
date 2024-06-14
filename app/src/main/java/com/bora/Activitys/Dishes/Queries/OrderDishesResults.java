e0eebd4702ce9a38e46d3a2e31a80a93e61f7338
Dudu! <83459496+dudurtg2@users.noreply.github.com>
2024-06-14 16:47:57 -0300

    lista todos os pedidos para os adm

--- app/src/main/java/com/bora/Activitys/Dishes/Queries/OrderDishesResults.java
+++ app/src/main/java/com/bora/Activitys/Dishes/Queries/OrderDishesResults.java
@@ -9,24 +9,51 @@ import com.bora.Activitys.Main.MainActivity;
 import com.bora.Functions.DAO.Order.Queries.QueryDAO;
 import com.bora.Functions.DAO.Order.Queries.View.AdapterViewOrder;
 import com.bora.databinding.ActivityDishesOrderBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class OrderDishesResults extends AppCompatActivity {
     private ActivityDishesOrderBinding binding;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
         binding = ActivityDishesOrderBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.OrderDishesButton.setOnClickListener(v -> { startActivity(new Intent(this, MainActivity.class)); });
+        binding.OrderDishesButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+        });
         QueryItems();
     }
 
     public void QueryItems() {
-        QueryDAO OrderDishesDAO = new QueryDAO(this);
-        OrderDishesDAO.readData(orderDishesDAO -> {
-            binding.OrderTableResultDishesShow.setLayoutManager(new LinearLayoutManager(this));
-            binding.OrderTableResultDishesShow.setAdapter(new AdapterViewOrder(getApplicationContext(), orderDishesDAO));
-        });
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+            db.collection("usuarios").document(uid).get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    boolean admin = false;
+                    if (document != null && document.exists()) {
+                        admin = document.getBoolean("admin");
+                    }
+                    QueryDAO orderDishesDAO = new QueryDAO(this, mAuth, admin);
+                    orderDishesDAO.readData(orderDishesList -> {
+                        binding.OrderTableResultDishesShow.setLayoutManager(new LinearLayoutManager(this));
+                        binding.OrderTableResultDishesShow.setAdapter(new AdapterViewOrder(getApplicationContext(), orderDishesList));
+                    });
+                }
+            });
+        }
     }
 }
+
--- app/src/main/java/com/bora/Functions/DAO/Order/Queries/QueryDAO.java
+++ app/src/main/java/com/bora/Functions/DAO/Order/Queries/QueryDAO.java
@@ -2,33 +2,50 @@ package com.bora.Functions.DAO.Order.Queries;
 
 import android.content.Context;
 import android.widget.Toast;
-
 import com.bora.Functions.DTO.Dishes.DishesDTO;
-import com.bora.Functions.DTO.Users.UserDTO;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
-public class QueryDAO {
-    private Context context;
-    private FirebaseFirestore db;
-    private List<DishesDTO> dishesList;
+public
+class QueryDAO {
+    private
+    Context context;
+    private
+    FirebaseFirestore db;
+    private
+    List<DishesDTO> dishesList;
+    private
+    FirebaseAuth mAuth;
+    private
+    boolean admin;
 
-    public QueryDAO(Context context) {
+    public
+    QueryDAO(Context context, FirebaseAuth mAuth, boolean admin) {
         this.context = context;
         this.db = FirebaseFirestore.getInstance();
         this.dishesList = new ArrayList<>();
+        this.mAuth = mAuth;
+        this.admin = admin;
     }
 
-    public interface FirestoreCallback { void onCallback(List<DishesDTO> dishesList); }
-    public void readData(final FirestoreCallback firestoreCallback) {
-        db.collection("pedidos").get().addOnCompleteListener(task -> {
+    public
+    interface FirestoreCallback {
+        void onCallback(List<DishesDTO> dishesList);
+    }
+    public
+    void readData(final FirestoreCallback firestoreCallback) {
+        db.collection("pedidos").get().addOnCompleteListener(task->{
             if (task.isSuccessful()) {
                 dishesList.clear();
                 for (QueryDocumentSnapshot document : task.getResult()) {
-                    DishesDTO usuario = new DishesDTO(document.getString("nome_cliente"), document.getString("nome_prato"), document.getString("uid_cliente"), document.getString("uid_prato"), document.getString("data_pedido"));
-                    dishesList.add(usuario);
+                    String uidCliente = document.getString("uid_cliente");
+                    if (admin || uidCliente.equals(mAuth.getUid())) {
+                        DishesDTO usuario = new DishesDTO(document.getString("nome_cliente"), document.getString("nome_prato"), uidCliente, document.getString("uid_prato"), document.getString("data_pedido"));
+                        dishesList.add(usuario);
+                    }
                 }
                 firestoreCallback.onCallback(dishesList);
             } else {
